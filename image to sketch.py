2db6b344c1c840c7d340ac9257f7f59c4cc7e86b
0123-hamn <hemantaghosh009@gmail.com>
2025-06-04 21:26:59 +0530

    Update image to sketch.py

--- image to sketch.py
+++ image to sketch.py
@@ -1,5 +1,7 @@
 import cv2
+import os
 image = cv2.imread(r'C:\Users\HEMANTA\OneDrive\Desktop\hemanta cv.jpg')
+
 if image is None:
     print("Image not found. Please check the path.")
 else:
@@ -8,5 +10,6 @@ else:
     blur = cv2.GaussianBlur(invert, (21, 21), 0)
     inverted_blur = cv2.bitwise_not(blur)
     sketch = cv2.divide(gray_img, inverted_blur, scale=256.0)
-    cv2.imwrite('sketch.png', sketch)
-    print("Sketch saved as sketch2.png")
+    desktop_path = os.path.join(os.path.expanduser("~"), "Desktop", "sketch2.png")
+    cv2.imwrite( r'C:\Users\HEMANTA\OneDrive\Desktop\sketch2.png', sketch)
+    print(r"Sketch saved as C:\Users\HEMANTA\Desktop\sketch2.png")
